0ec0b2bf4778d3c12ff6250ad008b80fe1b8c12c
Ayrtonmcn <ayrtonmcn@gmail.com>
2021-05-27 00:06:52 -0300

    Melhorando ainda mais o Desafio 04

--- Desafio_04/js/main.js
+++ Desafio_04/js/main.js
@@ -1,29 +1,11 @@
 let valor = Number(gets());
-let nota100 = nota50 = nota20 = nota10 = nota5 = nota2 = nota1 = 0;
+let cedulas = [100, 50, 20, 10, 5, 2, 1];
+let quantidade = 0;
 
 console.log(valor);
 
-if (valor > 1) {
-  nota100 = Math.floor(valor / 100);
-  valor = valor % 100;
-  nota50 = Math.floor(valor / 50);
-  valor = valor % 50;
-  nota20 = Math.floor(valor / 20);
-  valor = valor % 20;
-  nota10 = Math.floor(valor / 10);
-  valor = valor % 10;
-  nota5 = Math.floor(valor / 5);
-  valor = valor % 5;
-  nota2 = Math.floor(valor / 2);
-  valor = valor % 2;
-  nota1 = Math.floor(valor / 1);
-  valor = valor % 1;
-}
-
-console.log(nota100 + ' nota(s) de R$ 100,00');
-console.log(nota50 + ' nota(s) de R$ 50,00');
-console.log(nota20 + ' nota(s) de R$ 20,00');
-console.log(nota10 + ' nota(s) de R$ 10,00');
-console.log(nota5 + ' nota(s) de R$ 5,00');
-console.log(nota2 + ' nota(s) de R$ 2,00');
-console.log(nota1 + ' nota(s) de R$ 1,00');
+for (i = 0; i < 7; i++) {
+    quantidade = Math.floor(valor / cedulas[i]);
+    valor = valor % cedulas[i];
+    console.log(`${quantidade} nota(s) de R$ ${cedulas[i]},00`);
+} 
